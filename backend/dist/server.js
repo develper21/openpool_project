8c6b39cfcc796d3a36d466587ec943570537449b
Narvin Sachaniya <githubnarvin@gmail.com>
2026-03-13 22:49:53 +0530

    Add backend/dist/server.js

---
+++ backend/dist/server.js
@@ -0,0 +1,17 @@
+"use strict";
+var __importDefault = (this && this.__importDefault) || function (mod) {
+    return (mod && mod.__esModule) ? mod : { "default": mod };
+};
+Object.defineProperty(exports, "__esModule", { value: true });
+const app_1 = __importDefault(require("./app"));
+const env_1 = require("./config/env");
+const PORT = env_1.env.PORT;
+const server = app_1.default.listen(PORT, () => {
+    console.log(`Server is running in ${env_1.env.NODE_ENV} mode on port ${PORT}`);
+});
+process.on('SIGTERM', () => {
+    console.log('SIGTERM signal received: closing HTTP server');
+    server.close(() => {
+        console.log('HTTP server closed');
+    });
+});
