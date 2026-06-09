dc27ce458145589d162064c385fc12b9c1bc32c0
Narvin Sachaniya <githubnarvin@gmail.com>
2026-06-09 12:04:38 +0530

    Remove backend folder - delete debug script

--- backend/debug.js
+++
@@ -1,9 +0,0 @@
-const { execSync } = require('child_process');
-const fs = require('fs');
-
-try {
-  execSync('npx prisma validate', { stdio: 'pipe' });
-  fs.writeFileSync('prisma_err.json', JSON.stringify({ success: true }));
-} catch (e) {
-  fs.writeFileSync('prisma_err.json', JSON.stringify({ stdout: e.stdout.toString(), stderr: e.stderr.toString() }));
-}
